ac31a397ea374726558bdddb202cb58312e39a2f
Kaustabh <191712959+chkrb@users.noreply.github.com>
2026-08-05 00:12:31 +0530

    feat(kt/DialogCards): add RadioButtonDialogCard

--- app/src/main/kotlin/io/github/easeatten/ui/common/DialogCards.kt
+++ app/src/main/kotlin/io/github/easeatten/ui/common/DialogCards.kt
@@ -15,10 +15,13 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -76,6 +79,43 @@ fun <T> RadioButtonDialogCard(
             }
 
             Spacer(modifier = Modifier.padding(vertical = 10.dp))
+        }
+    }
+}
+
+@Composable
+fun ContinuousSliderDialogCard(
+    modifier: Modifier = Modifier,
+    title: String,
+    value: Float,
+    onValueChange: (Float) -> Unit,
+    valueToLabel: (Float) -> String,
+) {
+    var value by remember { mutableStateOf(value) }
+
+    ElevatedCard(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(20.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(text = title, style = MaterialTheme.typography.titleLarge)
+                Text(text = valueToLabel(value))
+            }
+
+            Slider(
+                modifier = Modifier.fillMaxWidth(),
+                value = value,
+                onValueChange = { value = it },
+                onValueChangeFinished = { onValueChange(value) },
+            )
         }
     }
 }
